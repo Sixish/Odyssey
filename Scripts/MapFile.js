eaf974c5f42fa4237c47eca82be47bcaaeba67d8
Sixish <6Sixorish@gmail.com>
2015-07-19 22:47:57 +0800

    linting

--- Scripts/MapFile.js
+++ Scripts/MapFile.js
@@ -1,4 +1,6 @@
 "use strict";
+/* jshint bitwise: false */
+/*global Dat, Matrix3D */
 // Utility functions
 function padLeft(str, width, padding) {
 	while (str.length < width) {
@@ -6,7 +8,6 @@ function padLeft(str, width, padding) {
 	}
 	return str;
 }
-
 // We cannot read bytes in hexadecimal strings,
 // so we use half bytes
 var HALF_BYTES_POSX = 4,
@@ -18,7 +19,6 @@ var HALF_BYTES_POSX = 4,
 	HALF_BYTES_ITEMSPLASH = 2,
 	HALF_BYTES_ITEMFLUID = 2,
 	HALF_BYTES_ITEMFRAME = 2;
-
 function MapFile(x, y, z) {
 	// Each MapFile contains (256 x 256 x 1), enforce it.
 	console.assert(x <= 0xFF, 'MapFile has invalid x. Must be between 0 and 255. x=' + x);
@@ -28,8 +28,7 @@ function MapFile(x, y, z) {
 	this.baseX = (x & 0xFF);
 	this.baseY = (y & 0xFF);
 	this.baseZ = (z & 0xF);
-	
-	// Tile structure (flat array, y << 8 + x)
+	// Flat array of tiles
 	this.structure = [];
 }
 MapFile.getFileX = function (posx) {
@@ -53,7 +52,6 @@ MapFile.getFilename = function (fposx, fposy, fposz) {
 };
 MapFile.resolveTileOffset = function (x, y) {
 	return ((x & 0xFF) << 0) + ((y & 0xFF) << 8);
-	//return (x - ((MapFile.getFileX(x) & 0xFF) << 0)) + (y - ((MapFile.getFileY(y) & 0xFF) << 8));
 };
 MapFile.prototype.toString = function () {
 	return MapFile.getFilename(this.baseX, this.baseY, this.baseZ);
@@ -63,8 +61,7 @@ MapFile.prototype.addTile = function (x, y, mapTile) {
 	console.assert(mapTile instanceof MapTile, 'MapFile.addTile requires a MapTile');
 	this.structure[offset] = mapTile;
 };
-
-// Each MapTile contains an instance of a tile sight
+// Each MapTile contains an instance of a tile
 function MapTile() {
 	this.items = [];
 }
@@ -85,13 +82,12 @@ MapTile.prototype.addItem = function (mapItem) {
 function MapItem() {
 	this.id = 0;
 }
-
 // Result of a MapFileParser
 function MapFileParserResult() {
 	this.mapFiles = [];
 }
 MapFileParserResult.getMapOffset = function (posx, posy, posz) {
-	return (((fposx >> 8) & 0xFF) << 0) + (((fposy >> 8) & 0xFF) << 8) + (((fposz >> 0) & 0xFF) << 16);
+	return (((posx >> 8) & 0xFF) << 0) + (((posy >> 8) & 0xFF) << 8) + (((posz >> 0) & 0xFF) << 16);
 };
 MapFileParserResult.resolveIndex = function (fposx, fposy, fposz) {
 	return ((fposx & 0xFF) << 0) + ((fposy & 0xFF) << 8) + ((fposz & 0xFF) << 16);
@@ -112,15 +108,14 @@ function MapFileParser() {
 	this.tilePosition = new Matrix3D(0, 0, 0);
 }
 MapFileParser.prototype.parse = function (str) {
-	var result = new MapFileParserResult(), map, b, len, i, tiles, tile, hexString = '', filename, itemCount = '';
-	
+	var result = new MapFileParserResult(), map, b, len, i, tile, hexString = '', itemCount = '';
 	var posx, posy, posz;
 	var fposx, fposy, fposz;
 	var tposx, tposy, tposz;
 	var t, item;
 	b = 0;
 	len = str.length;
-	
+	// Parse the entire string
 	while (b < len) {
 		/* Parse Position */
 		// PosX
@@ -129,26 +124,23 @@ MapFileParser.prototype.parse = function (str) {
 		}
 		posx = parseInt(hexString, 16);
 		hexString = '';
-		
 		// PosY
 		for (i = 0; i < HALF_BYTES_POSY; i += 1, b += 1) {
 			hexString += str.charAt(b);
 		}
 		posy = parseInt(hexString, 16);
 		hexString = '';
-		
 		// PosZ
 		for (i = 0; i < HALF_BYTES_POSZ; i += 1, b += 1) {
 			hexString += str.charAt(b);
 		}
 		posz = parseInt(hexString, 16);
 		hexString = '';
-		
 		// Create a new map, if we don't already have one...
 		fposx = MapFile.getFileX(posx);
 		fposy = MapFile.getFileY(posy);
 		fposz = MapFile.getFileZ(posz);
-		
+		// Get the map file for position or create a new one
 		if (!result.hasMapFile(fposx, fposy, fposz)) {
 			if (fposz > 0xF) {
 				console.log(fposz, b);
@@ -157,31 +149,30 @@ MapFileParser.prototype.parse = function (str) {
 			result.addMapFile(map);
 		}
 		map = result.getMapFile(fposx, fposy, fposz);
-		
+		// Get the tile position of posx,y,z
 		tposx = MapTile.getTileX(posx);
 		tposy = MapTile.getTileY(posy);
 		tposz = MapTile.getTileZ(posz);
-		
 		// Read the amount of items on the tile
 		for (i = 0; i < HALF_BYTES_ITEMS_ON_TILE; i += 1, b += 1) {
 			hexString += str.charAt(b);
 		}
 		itemCount = parseInt(hexString, 16);
 		hexString = '';
-		
+		// Create the tile we will be using
 		tile = new MapTile();
 		map.addTile(tposx, tposy, tile);
-		
+		// Iterate over the items, adding each item to the tile
 		for (t = 0; t < itemCount; t += 1) {
 			item = new MapItem();
 			tile.addItem(item);
-			
+			// Get the item ID
 			for (i = 0; i < HALF_BYTES_ITEMID; i += 1, b += 1) {
 				hexString += str.charAt(b);
 			}
 			item.id = parseInt(hexString, 16);
 			hexString = '';
-			
+			// Query the Dat: is the item stackable?
 			if (Dat.isStackable(item.id)) {
 				for (i = 0; i < HALF_BYTES_ITEMCOUNT; i += 1, b += 1) {
 					hexString += str.charAt(b);
@@ -189,7 +180,7 @@ MapFileParser.prototype.parse = function (str) {
 				item.count = parseInt(hexString, 16);
 				hexString = '';
 			}
-			
+			// Query the Dat: is the item a splash?
 			if (Dat.isSplash(item.id)) {
 				for (i = 0; i < HALF_BYTES_ITEMSPLASH; i += 1, b += 1) {
 					hexString += str.charAt(b);
@@ -197,7 +188,7 @@ MapFileParser.prototype.parse = function (str) {
 				item.splash = parseInt(hexString, 16);
 				hexString = '';
 			}
-			
+			// Query the Dat: is the item a fluid?
 			if (Dat.isFluid(item.id)) {
 				for (i = 0; i < HALF_BYTES_ITEMFLUID; i += 1, b += 1) {
 					hexString += str.charAt(b);
@@ -205,7 +196,7 @@ MapFileParser.prototype.parse = function (str) {
 				item.fluid = parseInt(hexString, 16);
 				hexString = '';
 			}
-			
+			// Query the Dat: is the item animated?
 			if (Dat.isAnimated(item.id)) {
 				for (i = 0; i < HALF_BYTES_ITEMFRAME; i += 1, b += 1) {
 					hexString += str.charAt(b);
